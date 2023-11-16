8b8ca67bdfb0c3033cdb65cb8e2b5574f7760248
Sergey Napiev <napiev@mail.ru>
2023-11-16 19:38:50 +0300

    15PR feat front base_url

--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -1,5 +1,5 @@
 // export const BASE_URL = 'https://auth.nomoreparties.co';
-export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL = 'https://api.sergeynapiev.nomoredomainsmonster.ru';
 
 export const register = (password, email) => {
     return fetch(`${BASE_URL}/signup`, {
